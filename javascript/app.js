00644d7ec239f4fdd5471263d677ead84b94bd6e
hofman10 <nbijelic29@gmail.com>
2026-05-14 21:58:45 +0200

    add 3d card hover effect

--- javascript/app.js
+++ javascript/app.js
@@ -15,3 +15,29 @@ navLinks.forEach((link) => {
     }
   });
 });
+
+// Mouse tracking for Recent Games cards
+const recentGamesCards = document.querySelectorAll("#recent-games .box");
+
+recentGamesCards.forEach((card) => {
+  const cardInner = card.querySelector(".box-inner");
+
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    cardInner.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    cardInner.style.transform =
+      "perspective(1000px) rotateX(0deg) rotateY(0deg)";
+  });
+});
